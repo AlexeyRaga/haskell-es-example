a3f10eaef01a55dfab7cb7bf25d14bed8e06a227
Alexey Raga <alexey.raga@gmail.com>
2015-11-02 20:02:15 +1100

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -7,9 +7,6 @@ import Model.Card
 import Repository
 import Text.Read
 
-(|>) :: a -> (a -> b) -> b
-infixl 5 |>; (|>) a f = f a
-
 cardId :: CardId
 cardId = CardId $ pack "test-card-1"
 
@@ -22,20 +19,23 @@ main = do
   putStrLn ""
   putStrLn "Enter your commands here:"
   go conn card
+
   where
+
+    execLine conn card line =
+      case readMaybe line of
+        Just cmd -> execCmd conn card cmd
+        Nothing  -> return card
+
     go conn card = do
       line <- getLine
       case line of
         "exit"   -> putStrLn "Bye."
-        "expose" -> (putStrLn $ show card) >> go conn card
-        _        -> do
-          nextCard <- case readMaybe line of
-                        Just cmd -> exec conn card cmd
-                        Nothing  -> return card
-          go conn nextCard
+        "expose" -> (putStrLn $ show card)  >>  go conn card
+        _        -> execLine conn card line >>= go conn
 
-exec :: Connection -> Card -> Command Card -> IO Card
-exec conn card cmd =
+execCmd :: Connection -> Card -> Command Card -> IO Card
+execCmd conn card cmd =
   case handle card cmd of
     Left err           -> (putStrLn $ "--! " ++ show err) >> return card
     Right (newCard, e) -> do
